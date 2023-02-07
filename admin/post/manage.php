6617c7bd50d8ef04741d90c56d08932cfcfa4218
oluwaCyna <oluwashinaayomi2019@gmail.com>
2023-02-07 05:28:20 -0800

    Live Updates

--- admin/post/manage.php
+++ admin/post/manage.php
@@ -27,7 +27,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="/blog" class="brand-link">
+      <a href="/" class="brand-link">
         <img src="../../vendor/AdminLTE-3.2.0/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">MyBlogWebsite</span>
       </a>
